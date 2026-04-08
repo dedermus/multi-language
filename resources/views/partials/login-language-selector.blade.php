9daa30d66b7a452cd03efa857582e372679cf515
Alexander.L <lomovtsev.ab@plinor.ru>
2026-04-08 11:36:19 +0300

    Изменения обращения по пути перевода

--- resources/views/partials/login-language-selector.blade.php
+++ resources/views/partials/login-language-selector.blade.php
@@ -1,6 +1,6 @@
 {{-- Это будет вставлено в @section('admin.login.language_selector') --}}
 <div class="mb-3">
-    <label for="locale" class="form-label">{{ __('multi-language.languages_list') }}</label>
+    <label for="locale" class="form-label">{{ __('multi-language::multi-language.languages_list') }}</label>
     <select class="form-control" id="locale">
         @foreach($languages as $key => $language)
             <option value="{{$key}}" {!! $key != $current ?: 'selected' !!}>{{$language}}</option>
